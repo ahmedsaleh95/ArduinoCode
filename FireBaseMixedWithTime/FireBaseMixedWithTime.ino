edaa5dc4114cf088dee6a511a08779b464355f38
asz <ahmedsaleh_95@yahoo.com>
2018-12-20 20:46:04 +0200

    schedule and httpclient for timer

--- FireBaseMixedWithTime/FireBaseMixedWithTime.ino
+++ FireBaseMixedWithTime/FireBaseMixedWithTime.ino
@@ -3,6 +3,8 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <time.h>
+#include <ESP8266HTTPClient.h>
+
 
 
 // Set these to run example.
@@ -17,7 +19,7 @@ Adafruit_SSD1306 display(OLED_RESET);
 char buffer[80];
 int timezone = 3;
 int dst = 0;
-String LedStatus , timer;
+String LedStatus , timer , scheduleStart , scheduleEnd;
 const int PIN_LED = 14;
 bool timerflag;
 FirebaseArduino FirebaseSet;
@@ -56,6 +58,10 @@ void setup() {
 
   //get value of timer here
   timer = Firebase.getString("AAjT5c028d47dd69e/timer");
+  //get value of schedule end here
+  scheduleEnd = Firebase.getString("AAjT5c028d47dd69e/scheduleEnd");
+  //get value of schedule start here
+  scheduleStart = Firebase.getString("AAjT5c028d47dd69e/scheduleStart");
 
   timerflag = false;
   Serial.println("aaaaaaaaaaaaaaaaaa" + timer);
@@ -92,55 +98,100 @@ void loop() {
     LedStatus = String(!digitalRead(PIN_LED));
     timerflag = true;
     FirebaseSet.setString("AAjT5c028d47dd69e/status" , LedStatus);
-  }
+    //send request to server
+    HTTPClient http;
+    http.begin("kingofdarknessahmed.000webhostapp.com" , 80 , "/api/timer/turn?qrcode=AAjT5c028d47dd69e&status="+LedStatus);
 
-  if (LedStatus == "1")
-  {
-    digitalWrite(PIN_LED, HIGH);
-    Serial.println("it is on yaaay");
-  }
-  else if (LedStatus == "0")
-  {
-    digitalWrite(PIN_LED, LOW);
-    Serial.println("it is off oOps");
-  }
-
-  if (Firebase.available()) {
-    FirebaseObject event = Firebase.readEvent();
-    String eventType = event.getString("type");
-    eventType.toLowerCase();
-
-    Serial.print("event: ");
-    Serial.println(eventType);
-    if (eventType == "put") {
-      Serial.print("path: ");
-      Serial.println(event.getString("path"));
-      Serial.print("data: ");
-      Serial.println(event.getString("data"));
-      String path = event.getString("path");
-      String data = event.getString("data");
-      if (path == "/status")
-      {
-        LedStatus = data;
-        digitalWrite(PIN_LED, data.toInt());
+    Serial.print("[HTTP] GET...\n");
+    // start connection and send HTTP header
+    int httpCode = http.GET();
+    // httpCode will be negative on error
+    if (httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+      // file found at server
+      if (httpCode == HTTP_CODE_OK) {
+        String payload = http.getString();
+        Serial.println(payload);
       }
-      else if (path == "/timer")
-      {
-        timer = data;
-        timerflag = false;
-      }
-
-      display.clearDisplay();
-      display.setTextSize(2);
-      display.setTextColor(WHITE);
-      display.setCursor(0, 0);
-      display.println(path.c_str() + 1);
-      display.println(data);
-      display.display();
-      // check path if timer make variable equal timer else make variable equal ledStatus
-
+    } else {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
     }
+    http.end();
   }
-  delay(1000);
+
+
+if (scheduleStart == buffer)
+{
+  //    digitalWrite(PIN_LED, HIGH);
+  Serial.println("Schedule is set");
+  LedStatus = String(1);
+  FirebaseSet.setString("AAjT5c028d47dd69e/status" , LedStatus);
+}
+
+if (scheduleEnd == buffer)
+{
+  //    digitalWrite(PIN_LED, HIGH);
+  Serial.println("Schedule is Ended");
+  LedStatus = String(0);
+  FirebaseSet.setString("AAjT5c028d47dd69e/status" , LedStatus);
+}
+
+if (LedStatus == "1")
+{
+  digitalWrite(PIN_LED, HIGH);
+  Serial.println("it is on yaaay");
+}
+else if (LedStatus == "0")
+{
+  digitalWrite(PIN_LED, LOW);
+  Serial.println("it is off oOps");
+}
+
+if (Firebase.available()) {
+  FirebaseObject event = Firebase.readEvent();
+  String eventType = event.getString("type");
+  eventType.toLowerCase();
+
+  Serial.print("event: ");
+  Serial.println(eventType);
+  if (eventType == "put") {
+    Serial.print("path: ");
+    Serial.println(event.getString("path"));
+    Serial.print("data: ");
+    Serial.println(event.getString("data"));
+    String path = event.getString("path");
+    String data = event.getString("data");
+    if (path == "/status")
+    {
+      LedStatus = data;
+      digitalWrite(PIN_LED, data.toInt());
+    }
+    else if (path == "/timer")
+    {
+      timer = data;
+      timerflag = false;
+    }
+    else if (path == "/scheduleEnd")
+    {
+      scheduleEnd = data;
+    }
+    else if (path == "/scheduleStart")
+    {
+      scheduleStart = data;
+    }
+
+    display.clearDisplay();
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 0);
+    display.println(path.c_str() + 1);
+    display.println(data);
+    display.display();
+    // check path if timer make variable equal timer else make variable equal ledStatus
+
+  }
+}
+delay(1000);
 
 }
